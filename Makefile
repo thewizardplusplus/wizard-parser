de59e6cdcd826eda1613d215a8f7a9c39e7e0896
thewizardplusplus <thewizardplusplus@yandex.ru>
2014-09-26 21:12:49 +0400

    Make library; add test example.

--- Makefile
+++ Makefile
@@ -1,9 +1,16 @@
 SOURCE = source/thewizardplusplus/wizard_parser/
-OBJECTS = $(SOURCE)main.o $(SOURCE)parser/Parser.o $(SOURCE)node/Node.o
+OBJECTS = $(SOURCE)parser/Parser.o $(SOURCE)node/Node.o
+BUILD = build/
+BUILD_HEADERS = $(BUILD)headers/
+BUILD_LIBRARIES = $(BUILD)libraries/
 CXXFLAGS += -std=c++11 -Wpedantic -Wall -Wextra -O2
 
 main: $(OBJECTS)
-	$(CXX) $(OBJECTS) -o main
+	mkdir -p $(BUILD_LIBRARIES)
+	$(AR) crs $(BUILD_LIBRARIES)libWizardParser.a $(OBJECTS)
+
+	$(RM) -r $(BUILD_HEADERS)
+	rsync -r --include="*/" --include="*.h" --exclude="*" $(SOURCE) $(BUILD_HEADERS)
 
 clean:
 	find . -name "*.o" -type f -delete
---
+++ test/main.cpp
@@ -0,0 +1,49 @@
+#include <parser/Parser.h>
+#include <iostream>
+
+using namespace thewizardplusplus::wizard_parser::parser;
+
+/* Grammar description in EBNF:
+ *
+ * expression = disjunction;
+ * disjunction = conjunction, {"or", conjunction};
+ * conjunction = equality, {"and", equality};
+ * equality = comparison, {("==" | "/="), comparison};
+ * comparison = sum, {("<" | "<=" | ">" | ">="), sum};
+ * sum = product, {("+" | "-"), product};
+ * product = unary, {("*" | "/"), unary};
+ * unary = {"new" | "-" | "not"}, accessor;
+ * atom = number | ("(", expression, ")");
+ * number = ? /\d+(\.\d+)?/ ?;
+ */
+Parser grammar(void) {
+	const auto expression = nothing();
+
+	WP_RULE(number)
+		separation(nothing(), lexeme(+digit() >> !('.'_s >> +digit())))
+	WP_END
+	WP_RULE(atom) number | hide('('_s) >> expression >> hide(')'_s) WP_END
+	WP_RULE(unary) *(word("new"_t) | '-'_s | word("not"_t)) >> atom WP_END
+	WP_RULE(product) list(unary, '*'_s | '/'_s) WP_END
+	WP_RULE(sum) list(product, '+'_s | '-'_s) WP_END
+	WP_RULE(comparison) list(sum, lexeme(any("<>") >> !'='_s)) WP_END
+	WP_RULE(equality) list(comparison, "=="_t | "/="_t) WP_END
+	WP_RULE(conjunction) list(equality, hide(word("and"_t))) WP_END
+	WP_RULE(disjunction) list(conjunction, hide(word("or"_t))) WP_END
+	assign(expression, disjunction);
+
+	return separation(hide(*space()), expression);
+}
+
+int main(int number_of_arguments, char* arguments[]) try {
+	if (number_of_arguments == 1) {
+		throw std::invalid_argument("not specified expression");
+	}
+
+	const auto text = arguments[1];
+	const auto parser = grammar();
+	std::cout << parse(parser, text, SimplifyLevel::AST) << std::endl;
+} catch (std::exception& exception) {
+	std::cerr << "Error: \"" << exception.what() << "\"." << std::endl;
+	std::exit(EXIT_FAILURE);
+}
