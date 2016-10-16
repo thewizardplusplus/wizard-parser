466f305b1b14ee5e5d3274b7c3ac0f7f96a91301
thewizardplusplus <thewizardplusplus@yandex.ru>
2016-10-17 02:23:13 +0300

    Add the uniting header for parsers

---
+++ source/thewizardplusplus/wizard_parser/parser/parsers.hpp
@@ -0,0 +1,14 @@
+#ifndef THEWIZARDPLUSPLUS_WIZARD_PARSER_PARSER_PARSERS_HEADER
+#define THEWIZARDPLUSPLUS_WIZARD_PARSER_PARSER_PARSERS_HEADER
+
+#include "eoi_parser.hpp"
+#include "type_match_parser.hpp"
+#include "value_match_parser.hpp"
+#include "concatenation_parser.hpp"
+#include "alternation_parser.hpp"
+#include "repetition_parser.hpp"
+#include "optional_parser.hpp"
+#include "exception_parser.hpp"
+#include "hide_parser.hpp"
+#include "dummy_parser.hpp"
+#endif
