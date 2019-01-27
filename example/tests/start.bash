0c00ed1ae00cf811ad7c9e29b89f9a5da2ba7006
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-01-27 10:32:32 +0300

    Refactoring of the example: refactoring of the tests runner

--- example/tests/start.bash
+++ example/tests/start.bash
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
-set -e
+set -o errexit
 
 declare tests_group="all"
-
-declare -r script_name="`basename "$0"`"
-# you can't wrap options variable in a command call (e.g. "declare" or
-# "eval set --"), because it hides its exit code
-options="`
+declare -r script_name="$(basename "$0")"
+# it's necessary to separate a variable declaration and its definition
+# so that the declare command doesn't hide an exit code of a defining expression
+declare options
+options="$(
 	getopt \
 		--name "$script_name" \
 		--options "hg:" \
 		--longoptions "help,group:" \
 		-- "$@"
-`"
+)"
 eval set -- "$options"
-while [[ "$1" != "--" ]]
-do
+while [[ "$1" != "--" ]]; do
 	case "$1" in
 		"-h" | "--help")
 			echo "Usage:"
@@ -31,24 +30,15 @@ do
 			exit 0
 			;;
 		"-g" | "--group")
-			case "$2" in
-				"lexer")
-					tests_group="lexer"
-					;;
-				"parser")
-					tests_group="parser"
-					;;
-			esac
-
-			# the additional shift for the option parameter
-			shift
+			tests_group="$2"
+			shift # additional shift for the option parameter
 			;;
 	esac
 
 	shift
 done
 
-declare -r script_path="`dirname "$0"`"
+declare -r script_path="$(dirname "$0")"
 declare -r example_command=("$script_path/../example")
 case "$tests_group" in
 	"lexer")
@@ -61,6 +51,5 @@ case "$tests_group" in
 		COMMAND="${example_command[@]}" bats \
 			"$script_path"/lexer/*.bats \
 			"$script_path"/parser/*.bats
-
 		;;
 esac
