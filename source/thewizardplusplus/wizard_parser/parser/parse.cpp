0b000ed1178eeb1707151e013581b23b43de0db8
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-01-27 10:32:32 +0300

    Remove an overload of the parser::parse() function accepting a string

--- source/thewizardplusplus/wizard_parser/parser/parse.cpp
+++ source/thewizardplusplus/wizard_parser/parser/parse.cpp
@@ -1,12 +1,7 @@
 #include "parse.hpp"
 #include "../utilities/unexpected_token_exception.hpp"
 #include "../utilities/unexpected_eoi_exception.hpp"
-#include "../lexer/tokenize.hpp"
-#include <iterator>
 #include <limits>
-
-using namespace thewizardplusplus::wizard_parser::lexer;
-using namespace thewizardplusplus::wizard_parser::utilities;
 
 namespace thewizardplusplus::wizard_parser::parser {
 
@@ -17,22 +12,15 @@ ast_node parse(
 	const auto ast = rule->parse(tokens);
 	if (!ast.node) {
 		if (!ast.rest_tokens.empty()) {
-			throw unexpected_token_exception{ast.rest_tokens[0].offset};
+			throw utilities::unexpected_token_exception{ast.rest_tokens[0].offset};
 		} else {
-			throw unexpected_eoi_exception{std::numeric_limits<std::size_t>::max()};
+			throw utilities::unexpected_eoi_exception{
+				std::numeric_limits<std::size_t>::max()
+			};
 		}
 	}
 
 	return *ast.node;
-}
-
-ast_node parse(
-	const lexeme_group& lexemes,
-	const rule_parser::pointer& rule,
-	std::string code
-) {
-	auto tokens = tokenize(lexemes, code);
-	return parse(rule, tokens);
 }
 
 }
--- source/thewizardplusplus/wizard_parser/parser/parse.hpp
+++ source/thewizardplusplus/wizard_parser/parser/parse.hpp
@@ -4,19 +4,12 @@
 #include "rule_parser.hpp"
 #include "../lexer/token.hpp"
 #include "ast_node.hpp"
-#include "../lexer/lexeme.hpp"
-#include <string>
 
 namespace thewizardplusplus::wizard_parser::parser {
 
 ast_node parse(
 	const rule_parser::pointer& rule,
 	const lexer::token_span& tokens
-);
-ast_node parse(
-	const lexer::lexeme_group& lexemes,
-	const rule_parser::pointer& rule,
-	std::string code
 );
 
 }
