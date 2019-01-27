4eeaaf710c46554be5fee978c6759ac89154b735
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-01-27 10:32:32 +0300

    Refactoring of the parser module: refactoring of the append_node() function

--- source/thewizardplusplus/wizard_parser/parser/common.cpp
+++ source/thewizardplusplus/wizard_parser/parser/common.cpp
@@ -1,21 +1,22 @@
 #include "common.hpp"
 #include <string>
 #include <unordered_set>
+#include <iterator>
+#include <algorithm>
 
 const auto ignorable_nodes = std::unordered_set<std::string>{"nothing", "eoi"};
 
 namespace thewizardplusplus::wizard_parser::parser {
 
 void append_node(ast_node_group& nodes, const ast_node& node) {
-	if (node.type == "sequence") {
-		for (const auto& child_node: node.children) {
-			if (ignorable_nodes.count(child_node.type) == 0) {
-				nodes.push_back(child_node);
-			}
-		}
-	} else if (ignorable_nodes.count(node.type) == 0) {
-		nodes.push_back(node);
-	}
+	const auto appended_nodes =
+		node.type == "sequence" ? node.children : ast_node_group{node};
+	std::copy_if(
+		std::cbegin(appended_nodes),
+		std::cend(appended_nodes),
+		std::back_inserter(nodes),
+		[&] (const auto& node) { return ignorable_nodes.count(node.type) == 0; }
+	);
 }
 
 }
