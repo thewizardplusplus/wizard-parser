617fcd112b3cce482cb9dc5e420b66e18a4f3bf5
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-02-04 01:19:12 +0300

    Refactoring of the `parser` module: refactoring of the `type_assignable_parser::parse()` method

--- source/thewizardplusplus/wizard_parser/parser/ast_node.hpp
+++ source/thewizardplusplus/wizard_parser/parser/ast_node.hpp
@@ -2,7 +2,6 @@
 #define THEWIZARDPLUSPLUS_WIZARD_PARSER_PARSER_AST_NODE_HEADER
 
 #include "../vendor/better-enums/enum_strict.hpp"
-#include "../vendor/enum-flags/flags.hpp"
 #include <cstdint>
 #include <string>
 #include <vector>
@@ -11,22 +10,10 @@ namespace thewizardplusplus::wizard_parser::parser {
 
 BETTER_ENUM(ast_node_type, std::uint8_t, sequence, nothing, eoi)
 
-enum class ast_node_flag: std::uint8_t {
-	none = 1 << 0,
-	named = 1 << 1
-};
-
-}
-
-ALLOW_FLAGS_FOR_ENUM(thewizardplusplus::wizard_parser::parser::ast_node_flag)
-
-namespace thewizardplusplus::wizard_parser::parser {
-
 struct ast_node {
 	std::string type;
 	std::string value;
 	std::vector<ast_node> children;
-	flags::flags<ast_node_flag> flags = ast_node_flag::none;
 };
 
 using ast_node_group = std::vector<ast_node>;
--- source/thewizardplusplus/wizard_parser/parser/type_assignable_parser.cpp
+++ source/thewizardplusplus/wizard_parser/parser/type_assignable_parser.cpp
@@ -18,15 +18,9 @@ parsing_result type_assignable_parser::parse(
 	const lexer::token_span& tokens
 ) const {
 	const auto ast = parser->parse(tokens);
-	if (!ast.node || ast.node->flags & ast_node_flag::named) {
-		return ast;
-	}
-
-	auto ast_copy = ast;
-	ast_copy.node->type = type;
-	ast_copy.node->flags = ast.node->flags | ast_node_flag::named;
-
-	return ast_copy;
+	return ast.node
+		? parsing_result{ast_node{type, {}, {*ast.node}}, ast.rest_tokens}
+		: ast;
 }
 
 }
