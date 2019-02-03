a3b286e08b6dd6aaf2da629a14504435fdde6d08
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-02-04 01:19:12 +0300

    Remove lifting: remove the `parser::lift_parser` class

--- source/thewizardplusplus/wizard_parser/parser/macroses.hpp
+++ source/thewizardplusplus/wizard_parser/parser/macroses.hpp
@@ -1,13 +1,11 @@
 #ifndef THEWIZARDPLUSPLUS_WIZARD_PARSER_PARSER_MACROSES_HEADER
 #define THEWIZARDPLUSPLUS_WIZARD_PARSER_PARSER_MACROSES_HEADER
 
-#include "lift_parser.hpp"
 #include "type_assignable_parser.hpp"
 #include <memory>
 
 #define RULE(type) const auto type = \
 	*std::make_shared< \
 		thewizardplusplus::wizard_parser::parser::type_assignable_parser \
-	>(#type) = \
-	*std::make_shared<thewizardplusplus::wizard_parser::parser::lift_parser>()
+	>(#type)
 #endif
