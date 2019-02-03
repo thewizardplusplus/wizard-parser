7bf4a1e679c126b0b7b1aa1b138740c57f75fa03
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-02-04 01:19:12 +0300

    Remove ignoring `nothing` and `eoi` nodes

--- source/thewizardplusplus/wizard_parser/parser/common.cpp
+++ source/thewizardplusplus/wizard_parser/parser/common.cpp
@@ -1,26 +1,18 @@
 #include "common.hpp"
-#include <string>
-#include <unordered_set>
 #include <iterator>
 #include <algorithm>
 
 namespace thewizardplusplus::wizard_parser::parser {
-
-const auto ignorable_nodes = std::unordered_set<std::string>{
-	(+ast_node_type::nothing)._to_string(),
-	(+ast_node_type::eoi)._to_string()
-};
 
 void append_node(ast_node_group& nodes, const ast_node& node) {
 	const auto appended_nodes =
 		node.type != (+ast_node_type::sequence)._to_string()
 			? ast_node_group{node}
 			: node.children;
-	std::copy_if(
+	std::copy(
 		std::cbegin(appended_nodes),
 		std::cend(appended_nodes),
-		std::back_inserter(nodes),
-		[&] (const auto& node) { return ignorable_nodes.count(node.type) == 0; }
+		std::back_inserter(nodes)
 	);
 }
 
