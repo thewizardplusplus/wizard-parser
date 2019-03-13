ba0da11938b5560a776a67cd3024438038dd3faf
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-03-13 03:18:21 +0300

    Add tests for the `parser` module (with mocks): fix tests of the `alternation_parser` class

--- tests/parser/alternation_parser.cpp
+++ tests/parser/alternation_parser.cpp
@@ -1,19 +1,38 @@
-#include "../common/common.hpp"
-#include "../../source/thewizardplusplus/wizard_parser/lexer/token.hpp"
 #include "../../source/thewizardplusplus/wizard_parser/parser/rule_parser.hpp"
 #include "../../source/thewizardplusplus/wizard_parser/parser/alternation_parser.hpp"
+#include "../../source/thewizardplusplus/wizard_parser/lexer/token.hpp"
+#include "../../source/thewizardplusplus/wizard_parser/parser/ast_node.hpp"
 #include "../vendor/catch/catch.hpp"
 #include "../vendor/fakeit/fakeit.hpp"
 
 TEST_CASE("parser::alternation_parser class", "[parser]") {
 	using namespace thewizardplusplus::wizard_parser;
 	using namespace thewizardplusplus::wizard_parser::parser::operators;
-	using namespace tests::common;
 
-	auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
-	const auto match = ast_from_token(tokens);
+	SECTION("without tokens") {
+		auto left_mock_parser = fakeit::Mock<parser::rule_parser>{};
+		fakeit::When(Method(left_mock_parser, parse))
+			.Return(parser::parsing_result{});
+		fakeit::Fake(Dtor(left_mock_parser));
+
+		auto right_mock_parser = fakeit::Mock<parser::rule_parser>{};
+		fakeit::When(Method(right_mock_parser, parse))
+			.Return(parser::parsing_result{});
+		fakeit::Fake(Dtor(right_mock_parser));
+
+		const auto alternation_parser =
+			parser::rule_parser::pointer{&left_mock_parser.get()}
+			| parser::rule_parser::pointer{&right_mock_parser.get()};
+		const auto [ast, rest_tokens] = alternation_parser->parse({});
+		CHECK(!ast.has_value());
+		CHECK(rest_tokens.empty());
+
+		fakeit::Verify(Method(left_mock_parser, parse)).Once();
+		fakeit::Verify(Method(right_mock_parser, parse)).Once();
+	}
 
 	SECTION("without matches") {
+		auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
 		auto left_mock_parser = fakeit::Mock<parser::rule_parser>{};
 		fakeit::When(Method(left_mock_parser, parse))
 			.Return(parser::parsing_result{{}, tokens});
@@ -36,8 +55,13 @@ TEST_CASE("parser::alternation_parser class", "[parser]") {
 	}
 
 	SECTION("with a left match") {
+		auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
 		auto left_mock_parser = fakeit::Mock<parser::rule_parser>{};
-		fakeit::When(Method(left_mock_parser, parse)).Return(match);
+		fakeit::When(Method(left_mock_parser, parse))
+			.Return(parser::parsing_result{
+				parser::ast_node{"one", "two", {}, 1},
+				lexer::token_span{tokens}.subspan(1)
+			});
 		fakeit::Fake(Dtor(left_mock_parser));
 
 		auto right_mock_parser = fakeit::Mock<parser::rule_parser>{};
@@ -50,21 +74,26 @@ TEST_CASE("parser::alternation_parser class", "[parser]") {
 			| parser::rule_parser::pointer{&right_mock_parser.get()};
 		const auto [ast, rest_tokens] = alternation_parser->parse(tokens);
 		REQUIRE(ast.has_value());
-		CHECK(*ast == *match.node);
-		CHECK(rest_tokens == match.rest_tokens);
+		CHECK(*ast == parser::ast_node{"one", "two", {}, 1});
+		CHECK(rest_tokens == lexer::token_span{tokens}.subspan(1));
 
 		fakeit::Verify(Method(left_mock_parser, parse)).Once();
 		fakeit::Verify(Method(right_mock_parser, parse)).Once();
 	}
 
 	SECTION("with a right match") {
+		auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
 		auto left_mock_parser = fakeit::Mock<parser::rule_parser>{};
 		fakeit::When(Method(left_mock_parser, parse))
 			.Return(parser::parsing_result{{}, tokens});
 		fakeit::Fake(Dtor(left_mock_parser));
 
 		auto right_mock_parser = fakeit::Mock<parser::rule_parser>{};
-		fakeit::When(Method(right_mock_parser, parse)).Return(match);
+		fakeit::When(Method(right_mock_parser, parse))
+			.Return(parser::parsing_result{
+				parser::ast_node{"one", "two", {}, 1},
+				lexer::token_span{tokens}.subspan(1)
+			});
 		fakeit::Fake(Dtor(right_mock_parser));
 
 		const auto alternation_parser =
@@ -72,22 +101,33 @@ TEST_CASE("parser::alternation_parser class", "[parser]") {
 			| parser::rule_parser::pointer{&right_mock_parser.get()};
 		const auto [ast, rest_tokens] = alternation_parser->parse(tokens);
 		REQUIRE(ast.has_value());
-		CHECK(*ast == *match.node);
-		CHECK(rest_tokens == match.rest_tokens);
+		CHECK(*ast == parser::ast_node{"one", "two", {}, 1});
+		CHECK(rest_tokens == lexer::token_span{tokens}.subspan(1));
 
 		fakeit::Verify(Method(left_mock_parser, parse)).Once();
 		fakeit::Verify(Method(right_mock_parser, parse)).Once();
 	}
 
 	SECTION("with left and right matches") {
-		const auto left_ast = ast_with_type(match, "five");
+		auto tokens = lexer::token_group{
+			{"one", "two", 1},
+			{"three", "four", 4},
+			{"five", "six", 8}
+		};
 		auto left_mock_parser = fakeit::Mock<parser::rule_parser>{};
-		fakeit::When(Method(left_mock_parser, parse)).Return(left_ast);
+		fakeit::When(Method(left_mock_parser, parse))
+			.Return(parser::parsing_result{
+				parser::ast_node{"one", "two", {}, 1},
+				lexer::token_span{tokens}.subspan(1)
+			});
 		fakeit::Fake(Dtor(left_mock_parser));
 
 		auto right_mock_parser = fakeit::Mock<parser::rule_parser>{};
 		fakeit::When(Method(right_mock_parser, parse))
-			.Return(ast_with_type(match, "six"));
+			.Return(parser::parsing_result{
+				parser::ast_node{"three", "four", {}, 4},
+				lexer::token_span{tokens}.subspan(2)
+			});
 		fakeit::Fake(Dtor(right_mock_parser));
 
 		const auto alternation_parser =
@@ -95,8 +135,8 @@ TEST_CASE("parser::alternation_parser class", "[parser]") {
 			| parser::rule_parser::pointer{&right_mock_parser.get()};
 		const auto [ast, rest_tokens] = alternation_parser->parse(tokens);
 		REQUIRE(ast.has_value());
-		CHECK(*ast == *left_ast.node);
-		CHECK(rest_tokens == left_ast.rest_tokens);
+		CHECK(*ast == parser::ast_node{"one", "two", {}, 1});
+		CHECK(rest_tokens == lexer::token_span{tokens}.subspan(1));
 
 		fakeit::Verify(Method(left_mock_parser, parse)).Once();
 		fakeit::Verify(Method(right_mock_parser, parse)).Once();
