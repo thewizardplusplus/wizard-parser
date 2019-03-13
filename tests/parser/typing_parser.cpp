82cb5f1b640c329dee3f300c51231e374b7915ef
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-03-13 03:18:21 +0300

    Add tests for the `parser` module (with mocks): fix tests of the `typing_parser` class

--- tests/parser/typing_parser.cpp
+++ tests/parser/typing_parser.cpp
@@ -1,19 +1,31 @@
 #define THEWIZARDPLUSPLUS_WIZARD_PARSER_PARSER_MACROSES
 
-#include "../common/common.hpp"
-#include "../../source/thewizardplusplus/wizard_parser/lexer/token.hpp"
 #include "../../source/thewizardplusplus/wizard_parser/parser/rule_parser.hpp"
 #include "../../source/thewizardplusplus/wizard_parser/parser/typing_parser.hpp"
+#include "../../source/thewizardplusplus/wizard_parser/lexer/token.hpp"
+#include "../../source/thewizardplusplus/wizard_parser/parser/ast_node.hpp"
 #include "../vendor/catch/catch.hpp"
 #include "../vendor/fakeit/fakeit.hpp"
 
 TEST_CASE("parser::typing_parser class", "[parser]") {
 	using namespace thewizardplusplus::wizard_parser;
-	using namespace tests::common;
 
-	auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
+	SECTION("without tokens") {
+		auto mock_parser = fakeit::Mock<parser::rule_parser>{};
+		fakeit::When(Method(mock_parser, parse))
+			.Return(parser::parsing_result{});
+		fakeit::Fake(Dtor(mock_parser));
+
+		RULE(typing_parser) = parser::rule_parser::pointer{&mock_parser.get()};
+		const auto [ast, rest_tokens] = typing_parser->parse({});
+		CHECK(!ast.has_value());
+		CHECK(rest_tokens.empty());
+
+		fakeit::Verify(Method(mock_parser, parse)).Once();
+	}
 
 	SECTION("without a match") {
+		auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
 		auto mock_parser = fakeit::Mock<parser::rule_parser>{};
 		fakeit::When(Method(mock_parser, parse))
 			.Return(parser::parsing_result{{}, tokens});
@@ -28,17 +40,20 @@ TEST_CASE("parser::typing_parser class", "[parser]") {
 	}
 
 	SECTION("with a match") {
-		const auto match = ast_from_token(tokens);
+		auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
 		auto mock_parser = fakeit::Mock<parser::rule_parser>{};
-		fakeit::When(Method(mock_parser, parse)).Return(match);
+		fakeit::When(Method(mock_parser, parse))
+			.Return(parser::parsing_result{
+				parser::ast_node{"one", "two", {}, 1},
+				lexer::token_span{tokens}.subspan(1)
+			});
 		fakeit::Fake(Dtor(mock_parser));
 
-		const auto expected_match = ast_with_type(match, "typing_parser");
 		RULE(typing_parser) = parser::rule_parser::pointer{&mock_parser.get()};
 		const auto [ast, rest_tokens] = typing_parser->parse(tokens);
 		REQUIRE(ast.has_value());
-		CHECK(*ast == *expected_match.node);
-		CHECK(rest_tokens == expected_match.rest_tokens);
+		CHECK(*ast == parser::ast_node{"typing_parser", {}, {{"one", "two", {}, 1}}});
+		CHECK(rest_tokens == lexer::token_span{tokens}.subspan(1));
 
 		fakeit::Verify(Method(mock_parser, parse)).Once();
 	}
