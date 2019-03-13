b59b793aa659c723861180bb89ec169f8e4696f4
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-03-13 03:18:21 +0300

    Add tests for the `lexer` module: add tests for the `find_matched_token()` function

---
+++ tests/lexer/find_matched_token.cpp
@@ -0,0 +1,70 @@
+#include "../../source/thewizardplusplus/wizard_parser/lexer/token.hpp"
+#include "../../source/thewizardplusplus/wizard_parser/lexer/lexeme.hpp"
+#include "../vendor/catch/catch.hpp"
+#include <optional>
+#include <string_view>
+#include <regex>
+
+namespace thewizardplusplus::wizard_parser::lexer {
+
+std::optional<token> find_matched_token(
+	const lexeme_group& lexemes,
+	const std::string_view& code
+);
+
+}
+
+TEST_CASE("lexer::find_matched_token() function", "[lexer]") {
+	using namespace thewizardplusplus::wizard_parser;
+
+	SECTION("success with one matching lexeme") {
+		const auto token = lexer::find_matched_token(
+			{
+				{std::regex{"[[:alpha:]]+"}, "alpha"},
+				{std::regex{"[[:digit:]]+"}, "digit"}
+			},
+			"23 tests"
+		);
+		REQUIRE(token.has_value());
+		CHECK(*token == lexer::token{"digit", "23"});
+	}
+
+	SECTION("success with few matching lexemes") {
+		const auto token = lexer::find_matched_token(
+			{
+				{std::regex{"[[:alnum:]]+"}, "alnum"},
+				{std::regex{"[[:digit:]]+"}, "digit"}
+			},
+			"23 tests"
+		);
+		REQUIRE(token.has_value());
+		CHECK(*token == lexer::token{"alnum", "23"});
+	}
+
+	SECTION("failure without matching lexemes") {
+		const auto token = lexer::find_matched_token(
+			{
+				{std::regex{"[[:alpha:]]+"}, "alpha"},
+				{std::regex{"[[:punct:]]+"}, "punct"}
+			},
+			"23 tests"
+		);
+		CHECK(!token.has_value());
+	}
+
+	SECTION("failure without lexemes") {
+		const auto token = lexer::find_matched_token({}, "23 tests");
+		CHECK(!token.has_value());
+	}
+
+	SECTION("failure without a code") {
+		const auto token = lexer::find_matched_token(
+			{
+				{std::regex{"[[:alpha:]]+"}, "alpha"},
+				{std::regex{"[[:digit:]]+"}, "digit"}
+			},
+			{}
+		);
+		CHECK(!token.has_value());
+	}
+}
