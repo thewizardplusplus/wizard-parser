d79975d4be62ba68788fc807f71c27fbf0505502
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-03-13 03:18:21 +0300

    Add tests for the `parser` module: fix tests of the `match_parser` class

--- tests/parser/match_parser.cpp
+++ tests/parser/match_parser.cpp
@@ -1,40 +1,38 @@
-#include "../common/common.hpp"
 #include "../../source/thewizardplusplus/wizard_parser/parser/match_parser.hpp"
 #include "../../source/thewizardplusplus/wizard_parser/lexer/token.hpp"
+#include "../../source/thewizardplusplus/wizard_parser/parser/ast_node.hpp"
 #include "../vendor/catch/catch.hpp"
 
 TEST_CASE("parser::match_parser class", "[parser]") {
 	using namespace thewizardplusplus::wizard_parser;
 	using namespace thewizardplusplus::wizard_parser::parser::operators;
-	using namespace tests::common;
 
-	const auto match_parser = "test"_t;
-	auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
-	const auto match = ast_from_token(tokens);
-
-	SECTION("not match without tokens") {
-		const auto [ast, rest_tokens] = match_parser->parse({});
+	SECTION("without tokens") {
+		const auto [ast, rest_tokens] = "test"_t->parse({});
 		CHECK(!ast.has_value());
 		CHECK(rest_tokens.empty());
 	}
 
-	SECTION("not match with tokens") {
-		const auto [ast, rest_tokens] = match_parser->parse(tokens);
+	SECTION("without a match") {
+		auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
+		const auto [ast, rest_tokens] = "test"_t->parse(tokens);
 		CHECK(!ast.has_value());
 		CHECK(rest_tokens == lexer::token_span{tokens});
 	}
 
-	SECTION("match by a type") {
+	SECTION("with a match by a type") {
+		auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
 		const auto [ast, rest_tokens] = "one"_t->parse(tokens);
 		REQUIRE(ast.has_value());
-		CHECK(*ast == *match.node);
-		CHECK(rest_tokens == match.rest_tokens);
+		CHECK(*ast == parser::ast_node{"one", "two", {}, 1});
+		CHECK(rest_tokens == lexer::token_span{tokens}.subspan(1));
 	}
 
-	SECTION("match by a value") {
+	SECTION("with a match by a value") {
+		auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
 		const auto [ast, rest_tokens] = "two"_v->parse(tokens);
 		REQUIRE(ast.has_value());
-		CHECK(*ast == *match.node);
-		CHECK(rest_tokens == match.rest_tokens);
+		CHECK(*ast == parser::ast_node{"one", "two", {}, 1});
+		CHECK(rest_tokens == lexer::token_span{tokens}.subspan(1));
 	}
 }
