f8a75b114088db91bfec80f5c89f87991070c408
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-03-13 03:18:21 +0300

    Add tests for the `parser` module (with mocks): fix tests of the `dummy_parser` class

--- tests/parser/dummy_parser.cpp
+++ tests/parser/dummy_parser.cpp
@@ -1,18 +1,20 @@
-#include "../common/common.hpp"
 #include "../../source/thewizardplusplus/wizard_parser/lexer/token.hpp"
 #include "../../source/thewizardplusplus/wizard_parser/parser/rule_parser.hpp"
+#include "../../source/thewizardplusplus/wizard_parser/parser/ast_node.hpp"
 #include "../../source/thewizardplusplus/wizard_parser/parser/dummy_parser.hpp"
 #include "../vendor/catch/catch.hpp"
 #include "../vendor/fakeit/fakeit.hpp"
 
 TEST_CASE("parser::dummy_parser class", "[parser]") {
 	using namespace thewizardplusplus::wizard_parser;
-	using namespace tests::common;
 
 	auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
-	const auto match = ast_from_token(tokens);
 	auto mock_parser = fakeit::Mock<parser::rule_parser>{};
-	fakeit::When(Method(mock_parser, parse)).Return(match);
+	fakeit::When(Method(mock_parser, parse))
+		.Return(parser::parsing_result{
+			parser::ast_node{"one", "two", {}, 1},
+			lexer::token_span{tokens}.subspan(1)
+		});
 	fakeit::Fake(Dtor(mock_parser));
 
 	const auto mock_parser_pointer =
@@ -22,8 +24,8 @@ TEST_CASE("parser::dummy_parser class", "[parser]") {
 
 	const auto [ast, rest_tokens] = dummy_parser->parse(tokens);
 	REQUIRE(ast.has_value());
-	CHECK(*ast == *match.node);
-	CHECK(rest_tokens == match.rest_tokens);
+	CHECK(*ast == parser::ast_node{"one", "two", {}, 1});
+	CHECK(rest_tokens == lexer::token_span{tokens}.subspan(1));
 
 	fakeit::Verify(Method(mock_parser, parse)).Once();
 }
