4750851911a979778fd50b1a60fa311c74e6e5cc
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-06-03 23:00:00 +0300

    Add the `exceptions` module, add the `positional_exception` class: implement it

---
+++ source/thewizardplusplus/wizard_parser/exceptions/positional_exception.cpp
@@ -0,0 +1,15 @@
+#include "positional_exception.hpp"
+#include "../vendor/fmt/format.hpp"
+
+namespace thewizardplusplus::wizard_parser::exceptions {
+
+positional_exception::positional_exception(
+	const std::string& description,
+	const std::size_t& offset
+):
+	std::runtime_error{fmt::format("{:s} (offset: {:d})", description, offset)},
+	description{description},
+	offset{offset}
+{}
+
+}
