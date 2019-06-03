e7a18cc69eafb484f152d57be2686835b6ebe460
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-06-03 23:00:00 +0300

    Add the `parser::parse_all()` function: implement it

--- source/thewizardplusplus/wizard_parser/parser/parse.cpp
+++ source/thewizardplusplus/wizard_parser/parser/parse.cpp
@@ -1,5 +1,6 @@
 #include "parse.hpp"
 #include "../vendor/range/v3/numeric/accumulate.hpp"
+#include "../exceptions/unexpected_entity_exception.hpp"
 
 namespace thewizardplusplus::wizard_parser::parser {
 
@@ -17,6 +18,25 @@ parsing_result parse(
 		ranges::accumulate(handlers, *ast.node, transformers::transform),
 		ast.rest_tokens
 	};
+}
+
+ast_node parse_all(
+	const rule_parser::pointer& parser,
+	const lexer::token_span& tokens,
+	const transformers::ast_node_handler_group& handlers
+) {
+	const auto ast = parse(parser, tokens, handlers);
+	const auto offset = lexer::get_offset(ast.rest_tokens);
+	if (!ast.rest_tokens.empty()) {
+		const auto type = exceptions::entity_type::token;
+		throw exceptions::unexpected_entity_exception<type>{offset};
+	}
+	if (!ast.node) {
+		const auto type = exceptions::entity_type::eoi;
+		throw exceptions::unexpected_entity_exception<type>{offset};
+	}
+
+	return *ast.node;
 }
 
 }
--- source/thewizardplusplus/wizard_parser/parser/parse.hpp
+++ source/thewizardplusplus/wizard_parser/parser/parse.hpp
@@ -16,6 +16,14 @@ parsing_result parse(
 		transformers::join_sequences
 	}
 );
+ast_node parse_all(
+	const rule_parser::pointer& parser,
+	const lexer::token_span& tokens,
+	const transformers::ast_node_handler_group& handlers = {
+		transformers::remove_nothings,
+		transformers::join_sequences
+	}
+);
 
 }
 #endif
