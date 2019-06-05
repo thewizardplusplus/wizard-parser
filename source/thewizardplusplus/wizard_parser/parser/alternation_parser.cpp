23118d46ff5dcb2b89ec54b5ede52275fcc9b461
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-06-05 23:00:00 +0300

    Fix error handling in the `parser::alternation_parser::parse()` method

--- source/thewizardplusplus/wizard_parser/parser/alternation_parser.cpp
+++ source/thewizardplusplus/wizard_parser/parser/alternation_parser.cpp
@@ -15,8 +15,21 @@ parsing_result alternation_parser::parse(
 	const lexer::token_span& tokens
 ) const {
 	const auto left_ast = left_parser->parse(tokens);
+	if (left_ast.node) {
+		return left_ast;
+	}
+
 	const auto right_ast = right_parser->parse(tokens);
-	return left_ast.node ? left_ast : right_ast;
+	if (right_ast.node) {
+		return right_ast;
+	}
+
+	// return a maximal offset, to which tokens can be parsed
+	const auto left_rest_offset = lexer::get_offset(left_ast.rest_tokens);
+	const auto right_rest_offset = lexer::get_offset(right_ast.rest_tokens);
+	return left_rest_offset >= right_rest_offset
+		? parsing_result{{}, left_ast.rest_tokens}
+		: parsing_result{{}, right_ast.rest_tokens};
 }
 
 namespace operators {
