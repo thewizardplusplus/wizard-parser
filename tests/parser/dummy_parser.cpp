8b5124acb6c68fd903a66b3a2a5ac3105a539ec2
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-06-07 23:00:00 +0300

    Improve tests of the `parser::dummy_parser` class

--- tests/parser/dummy_parser.cpp
+++ tests/parser/dummy_parser.cpp
@@ -1,31 +1,70 @@
-#include "../../source/thewizardplusplus/wizard_parser/lexer/token.hpp"
 #include "../../source/thewizardplusplus/wizard_parser/parser/rule_parser.hpp"
-#include "../../source/thewizardplusplus/wizard_parser/parser/ast_node.hpp"
 #include "../../source/thewizardplusplus/wizard_parser/parser/dummy_parser.hpp"
+#include "../../source/thewizardplusplus/wizard_parser/lexer/token.hpp"
+#include "../../source/thewizardplusplus/wizard_parser/parser/ast_node.hpp"
 #include "../vendor/catch/catch.hpp"
 #include "../vendor/fakeit/fakeit.hpp"
 
 TEST_CASE("parser::dummy_parser class", "[parser]") {
 	using namespace thewizardplusplus::wizard_parser;
 
-	auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
-	auto mock_parser = fakeit::Mock<parser::rule_parser>{};
-	fakeit::When(Method(mock_parser, parse))
-		.Return(parser::parsing_result{
-			parser::ast_node{"one", "two", {}, 1},
-			lexer::token_span{tokens}.subspan(1)
-		});
-	fakeit::Fake(Dtor(mock_parser));
+	SECTION("without tokens") {
+		auto mock_parser = fakeit::Mock<parser::rule_parser>{};
+		fakeit::When(Method(mock_parser, parse))
+			.Return(parser::parsing_result{});
+		fakeit::Fake(Dtor(mock_parser));
 
-	const auto mock_parser_pointer =
-		parser::rule_parser::pointer{&mock_parser.get()};
-	const auto dummy_parser = parser::dummy();
-	dummy_parser->set_parser(mock_parser_pointer);
+		const auto mock_parser_pointer =
+			parser::rule_parser::pointer{&mock_parser.get()};
+		const auto dummy_parser = parser::dummy();
+		dummy_parser->set_parser(mock_parser_pointer);
 
-	const auto [ast, rest_tokens] = dummy_parser->parse(tokens);
-	REQUIRE(ast.has_value());
-	CHECK(*ast == parser::ast_node{"one", "two", {}, 1});
-	CHECK(rest_tokens == lexer::token_span{tokens}.subspan(1));
+		const auto [ast, rest_tokens] = dummy_parser->parse({});
+		CHECK(!ast.has_value());
+		CHECK(rest_tokens.empty());
 
-	fakeit::Verify(Method(mock_parser, parse)).Once();
+		fakeit::Verify(Method(mock_parser, parse)).Once();
+	}
+
+	SECTION("without a match") {
+		auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
+		auto mock_parser = fakeit::Mock<parser::rule_parser>{};
+		fakeit::When(Method(mock_parser, parse))
+			.Return(parser::parsing_result{{}, lexer::token_span{tokens}.subspan(1)});
+		fakeit::Fake(Dtor(mock_parser));
+
+		const auto mock_parser_pointer =
+			parser::rule_parser::pointer{&mock_parser.get()};
+		const auto dummy_parser = parser::dummy();
+		dummy_parser->set_parser(mock_parser_pointer);
+
+		const auto [ast, rest_tokens] = dummy_parser->parse(tokens);
+		CHECK(!ast.has_value());
+		CHECK(rest_tokens == lexer::token_span{tokens}.subspan(1));
+
+		fakeit::Verify(Method(mock_parser, parse)).Once();
+	}
+
+	SECTION("with a match") {
+		auto tokens = lexer::token_group{{"one", "two", 1}, {"three", "four", 4}};
+		auto mock_parser = fakeit::Mock<parser::rule_parser>{};
+		fakeit::When(Method(mock_parser, parse))
+			.Return(parser::parsing_result{
+				parser::ast_node{"one", "two", {}, 1},
+				lexer::token_span{tokens}.subspan(1)
+			});
+		fakeit::Fake(Dtor(mock_parser));
+
+		const auto mock_parser_pointer =
+			parser::rule_parser::pointer{&mock_parser.get()};
+		const auto dummy_parser = parser::dummy();
+		dummy_parser->set_parser(mock_parser_pointer);
+
+		const auto [ast, rest_tokens] = dummy_parser->parse(tokens);
+		REQUIRE(ast.has_value());
+		CHECK(*ast == parser::ast_node{"one", "two", {}, 1});
+		CHECK(rest_tokens == lexer::token_span{tokens}.subspan(1));
+
+		fakeit::Verify(Method(mock_parser, parse)).Once();
+	}
 }
