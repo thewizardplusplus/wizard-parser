e2aadd294069241cbc44c4ed46da3002846f2af6
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-06-11 23:00:00 +0300

    Fix error handling in the `parser::lookahead_parser::parse()` method

--- source/thewizardplusplus/wizard_parser/parser/lookahead_parser.cpp
+++ source/thewizardplusplus/wizard_parser/parser/lookahead_parser.cpp
@@ -14,12 +14,19 @@ lookahead_parser::lookahead_parser(
 parsing_result lookahead_parser::parse(const lexer::token_span& tokens) const {
 	const auto type = (+ast_node_type::nothing)._to_string();
 	const auto ast = parser->parse(tokens);
+	// if a match is expected, return rest tokens from its result
+	// (i.e. an offset or after a match, or before its unexpected token)
+	//
+	// if a nonmatch is expected, return rest tokens without additional one
+	// (i.e. a current offset and for further parsing,
+	// and as an unexpected token position)
+	const auto rest_tokens = expected_result ? ast.rest_tokens : tokens;
 	return ast.node.has_value() == expected_result
 		? parsing_result{
 			ast_node{type, {}, {}, lexer::get_offset(tokens)},
-			ast.rest_tokens
+			rest_tokens
 		}
-		: parsing_result{{}, ast.rest_tokens};
+		: parsing_result{{}, rest_tokens};
 }
 
 namespace operators {
