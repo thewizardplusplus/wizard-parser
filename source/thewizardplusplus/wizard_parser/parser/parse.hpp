0c47a6cda93a703171e4875509799aba67ec7d42
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-06-17 23:00:00 +0300

    Add the `transformers::default_ast_node_handlers` constant: use it in the `parser::parse_all()` function

--- source/thewizardplusplus/wizard_parser/parser/parse.hpp
+++ source/thewizardplusplus/wizard_parser/parser/parse.hpp
@@ -4,7 +4,6 @@
 #include "rule_parser.hpp"
 #include "../lexer/token.hpp"
 #include "../transformers/transform.hpp"
-#include "../transformers/transformers.hpp"
 
 namespace thewizardplusplus::wizard_parser::parser {
 
@@ -17,10 +16,8 @@ parsing_result parse(
 ast_node parse_all(
 	const rule_parser::pointer& parser,
 	const lexer::token_span& tokens,
-	const transformers::ast_node_handler_group& handlers = {
-		transformers::remove_nothings,
-		transformers::join_sequences
-	}
+	const transformers::ast_node_handler_group& handlers
+		= transformers::default_ast_node_handlers
 );
 
 }
