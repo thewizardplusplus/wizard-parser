64e7114ce655023d9457589c339c7c030073b9f7
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-06-19 23:00:00 +0300

    Unifying header for all parsers: implement it

---
+++ source/thewizardplusplus/wizard_parser/parser/parsers.hpp
@@ -0,0 +1,13 @@
+#ifndef THEWIZARDPLUSPLUS_WIZARD_PARSER_PARSER_PARSERS_HEADER
+#define THEWIZARDPLUSPLUS_WIZARD_PARSER_PARSER_PARSERS_HEADER
+
+#include "alternation_parser.hpp"
+#include "concatenation_parser.hpp"
+#include "dummy_parser.hpp"
+#include "exception_parser.hpp"
+#include "list_parser.hpp"
+#include "lookahead_parser.hpp"
+#include "match_parser.hpp"
+#include "repetition_parser.hpp"
+#include "typing_parser.hpp"
+#endif
